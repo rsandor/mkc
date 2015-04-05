9594f02918eb7b6df5204dfb53b05eb3bda80545
Ryan Sandor Richards <sandor.richards@gmail.com>
2015-04-05 01:42:53 -0700

    Initial implementation.

--- index.js
+++ index.js
@@ -1,1 +1,144 @@
-throw new Error('not implemented');
+'use strict';
+
+var LRU = require('lru-cache');
+
+/**
+ * @module mkc
+ * @author Ryan Sandor Richards
+ */
+module.exports = MultiKeyCache;
+
+/**
+ * Multi-Key LRU cache.
+ * @class
+ * @param {object} options Options for the cache.
+ */
+function MultiKeyCache(options) {
+  options = options || {};
+
+  var self = this;
+  var dispose = options.dispose;
+
+  options.dispose = function (key, value) {
+    self._dispose(key);
+    if (dispose) { dispose(key, value); }
+  };
+
+  this.cache = new LRU(options);
+  this.keyMap = {};
+}
+
+/**
+ * Hashes a set of key values into a single key.
+ * @param {object} keyValues Key values to hash.
+ * @return {string} The hash string for the set of key values.
+ */
+function hash(keyValues) {
+  return JSON.stringify(keyValues);
+}
+
+/**
+ * Parses a hashed key value set.
+ * @param {string} key Key to parse.
+ * @return {object} Set of key values represented by the key.
+ */
+function parse(key) {
+  return JSON.parse(key);
+}
+
+/**
+ * Removes mappings when objects are disposed by the LRU.
+ * @param {string} keyHash Key hash for the disposed object.
+ */
+MultiKeyCache.prototype._dispose = function (keyHash) {
+  var keyValues = JSON.parse(keyHash);
+  for (var key in keyValues) {
+    var value = keyValues[key];
+    if (!this.keyMap[key]) {
+      continue;
+    }
+    if (!Array.isArray(this.keyMap[key][value])) {
+      continue;
+    }
+    var map = this.keyMap[key][value];
+    var index = map.indexOf(keyHash);
+    if (~index) {
+      map.splice(index, 1);
+    }
+  }
+}
+
+/**
+ * Puts an object into the cache with the given key values.
+ * @param {object} keyValues Key values that identify the object in the cache.
+ * @param object Object to store in the cache.
+ */
+MultiKeyCache.prototype.set = function (keyValues, object) {
+  var keyHash = hash(keyValues);
+
+  for (var key in keyValues) {
+    var value = keyValues[key];
+    if (!this.keyMap[key]) {
+      this.keyMap[key] = {};
+    }
+    if (!this.keyMap[key][value]) {
+      this.keyMap[key][value] = [];
+    }
+    this.keyMap[key][value].push(keyHash);
+  }
+
+  this.cache.set(keyHash, object);
+};
+
+/**
+ * Gets and object from the cache with the given key values.
+ * @param {Object} keyValues Key values that identify the object in the cache.
+ * @return If found, the object with the given key values. `null` otherwise.
+ */
+MultiKeyCache.prototype.get = function (keyValues) {
+  var keyHash = hash(keyValues);
+  return this.cache.get(keyHash);
+};
+
+/**
+ * Removes all objects with the given key values.
+ *
+ * @example
+ * // Add two objects
+ * cache.set({ type: 'string', id: 1 }, 'one');
+ * cache.set({ type: 'number', id: 1 }, 1);
+ *
+ * // Remove only objects with the key `type === 'string'`
+ * // (only the object with type number will remain...)
+ * cache.purge({ type: 'string' });
+ *
+ * @param {Object} keyValues Key values to identify the objects to remove.
+ */
+MultiKeyCache.prototype.purge = function (keyValues) {
+  var delKeyHashSet = {};
+
+  for (var key in keyValues) {
+    var value = keyValues[key];
+    if (!this.keyMap[key]) {
+      continue;
+    }
+    if (!Array.isArray(this.keyMap[key])) {
+      continue;
+    }
+    this.keyMap[key][value].forEach(function (keyHash) {
+      delKeyHashSet[keyHash] = true;
+    });
+  }
+
+  for (var keyHash in delKeyHashSet) {
+    this.cache.purge(keyHash);
+  }
+};
+
+/**
+ * Clears the entire cache.
+ */
+MultiKeyCache.prototype.reset = function () {
+  this.cache.reset();
+  this.keyMap = {};
+};
